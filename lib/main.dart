43bcbd354583455d6f6f8d233f1641117bbfab95
Perhenric <perhenric@MacBook-Pro-som-tillhor-Perhenric.local>
2023-10-24 09:51:50 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'dart:convert';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _catJoke = "Hejsan";
+
+  final dio = Dio();
+
+  void _getCatJoke() async {
+    Response response = await dio.get('https://cat-fact.herokuapp.com/facts');
+
+    var text = response.data[0]["text"];
+    setState(() {
+      _catJoke = text;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Push the button and get a cat joke',
+            ),
+            Text(
+              _catJoke,
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _getCatJoke,
+        tooltip: 'More cats',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
